6d9a6f4bcdf5ade8f779683f44e3a351104d40d7
Matt <mlea@schematical.com>
2019-09-06 16:11:19 -0500

    Playing with ChaosPixel - Starting to do deployments

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 class AuthService{
     static signup(username, password){
-        return axios.post('/user?ID=12345')
+        return axios.post('https://chaosnet.schematical.com/v0/auth/signup')
     }
     static login(username, password){
         console.log(username, password);
@@ -10,6 +10,15 @@ class AuthService{
            username:username,
            password: password
        })
+
+    }
+    static checkVerificationToken(username, password){
+        console.log(username, password);
+
+        return axios.post('https://chaosnet.schematical.com/v0/auth/login', {
+            username:username,
+            password: password
+        })
 
     }
 }
